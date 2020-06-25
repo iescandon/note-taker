2da4e3d1d7ee747b56890434f30cc26cc3c4039d
Inez Escandon <iescan4@gmail.com>
2020-06-25 17:36:55 -0500

    put asterisk path at the bottom of all other paths

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
     res.json(notes);
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.post("/api/notes", function (req, res) {
